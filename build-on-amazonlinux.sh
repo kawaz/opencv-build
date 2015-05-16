585b8bdbd6812195e16cc2c9d091dbe9b559cd5c
Yoshiaki Kawazu <kawazzz@gmail.com>
2015-05-17 04:28:06 +0900

    source_dirを指定できるようにした

--- build-on-amazonlinux.sh
+++ build-on-amazonlinux.sh
@@ -3,21 +3,18 @@
 install_prefix=${install_prefix:-/opt/opencv}
 branch=${branch:-2.4}
 build_dir=${build_dir:-/tmp/opencv_build."$(date +%Y%m%d%H%M%S)".$$}
+source_dir="${source_dir:-$build_dir/opencv-$branch}"
 
-mkdir -p "$build_dir"
-cd "$build_dir" || exit 1
 packages=(git cmake gcc-c++ libjpeg-turbo-devel libpng-devel libtiff-devel openmpi-devel tbb-devel)
 if ! rpm -q "${packages[@]}" >/dev/null 2>&1; then
   sudo yum -y install "${packages[@]}"
 fi
 
-git clone -b "$branch" https://github.com/Itseez/opencv.git opencv
-(cd opencv && git pull)
+mkdir -p "$build_dir" && cd "$build_dir" || exit 1
 
-rm -rf release
-mkdir -p release
-cd release
-
+git clone -b "$branch" https://github.com/Itseez/opencv.git "$source_dir"
+( cd "$source_dir" && git checkout "$branch" && git pull )
+rm -rf release && mkdir -p release && cd release || exit 1
 cmake \
   -D BUILD_EXAMPLES=ON \
   -D CMAKE_BUILD_TYPE=RELEASE \
@@ -25,6 +22,6 @@ cmake \
   -D WITH_1394=OFF \
   -D WITH_OPENMP=ON \
   -D WITH_TBB=ON \
-  ../opencv
+  $source_dir
 
 make && make && make install
